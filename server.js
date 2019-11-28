33f403bd7eb3c0ae7cf3c3e42d84fa34718550fd
prince776 <princemit776@gmail.com>
2019-11-28 13:53:32 +0530

    Preparing for deployment. Removed server folder. all server files are now in main dir

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const configs = require('../config/config.js')
+const configs = require('./config/config.js')
 const cors = require('cors');
 var cookieParser = require('cookie-parser')
 const path = require('path')
@@ -39,10 +39,10 @@ require('./routes/api/blogPost.js')(app);
 //Serve static assests if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 
 }
