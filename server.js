4a19b9e47b88cef4fb1f542bebb6a7a400b45d7f
prince776 <princemit776@gmail.com>
2019-12-01 20:03:31 +0530

    Fixing base_irl

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
     app.get("/service-worker.js", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "worker.js"));
+        res.sendFile(path.resolve(__dirname, "public", "server-worker.js"));
     });
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
